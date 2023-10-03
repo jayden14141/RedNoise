7d16136851f53d1e626ff782d79360cd2a03bb8d
jayden14141 <sd21099@bristol.ac.uk>
2023-10-03 17:13:47 +0100

    Finished Wk02

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -3,9 +3,22 @@
 #include <Utils.h>
 #include <fstream>
 #include <vector>
+#include <glm/glm.hpp>
 
 #define WIDTH 320
 #define HEIGHT 240
+
+std::vector<glm::vec3> interpolateThreeElementValues (glm::vec3 from, glm::vec3 to, int numberOfValues) {
+	std::vector<glm::vec3> v;
+	glm::vec3 dif = to - from;
+	glm::vec3 unit = dif / (float)(numberOfValues - 1);
+	// Scalar * vec3 is not accepted.
+	for(int i = 0; i < numberOfValues; i++) {
+		glm::vec3 a((float)i, (float)i, (float)i);
+		v.push_back(from + unit * a);
+	}
+	return v;
+}
 
 std::vector<float> interpolateSingleFloats(float from, float to, int numberOfValues) {
 	float unit = (to - from) / (float)(numberOfValues - 1);
@@ -25,12 +38,33 @@ void draw(DrawingWindow &window) {
 	// 		window.setPixelColour(x, y, colour);
 	// 	}
 	// }
-	std::vector<float> v = interpolateSingleFloats(255, 0, window.width);
+
+	// Wk2 Task03
+	// RGB (0,0,0) => Black || RGB (255, 255, 255) => White
+	// (0,0) => Top Left / (255, 255) => Bottom Right
+
+	// std::vector<float> v = interpolateSingleFloats(255, 0, window.width);
+	// for (size_t y = 0; y < window.height; y++) {
+	// 	float black = 0.0;
+	// 	for (size_t x = 0; x < window.width; x++) {
+	// 		black = v.at(x);
+	// 		uint32_t colour = (255 << 24) + (int(black) << 16) + (int(black) << 8) + int(black);
+	// 		window.setPixelColour(x, y, colour);
+	// 	}
+	// }
+
+	// Wk2 Task05
+	glm::vec3 topLeft(255, 0, 0);        // red 
+	glm::vec3 topRight(0, 0, 255);       // blue 
+	glm::vec3 bottomRight(0, 255, 0);    // green 
+	glm::vec3 bottomLeft(255, 255, 0);   // yellow
+
+	std::vector<glm::vec3> left = interpolateThreeElementValues(topLeft, bottomLeft, window.width);
+	std::vector<glm::vec3> right = interpolateThreeElementValues(topRight, bottomRight, window.width);
 	for (size_t y = 0; y < window.height; y++) {
-		float black = 0.0;
+		std::vector<glm::vec3> col = interpolateThreeElementValues(left[y], right[y], window.width);
 		for (size_t x = 0; x < window.width; x++) {
-			black = v.at(x);
-			uint32_t colour = (255 << 24) + (int(black) << 16) + (int(black) << 8) + int(black);
+			uint32_t colour = (255 << 24) + (int(col[x][0]) << 16) + (int(col[x][1]) << 8) + int(col[x][2]);
 			window.setPixelColour(x, y, colour);
 		}
 	}
@@ -53,10 +87,18 @@ int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
 
-	std::vector<float> result;
-	result = interpolateSingleFloats(2.2, 8.5, 7);
-	for(size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
-	std::cout << std::endl;
+	// std::vector<float> result;
+	// result = interpolateSingleFloats(2.2, 8.5, 7);
+	// for(size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
+
+	std::vector<glm::vec3> result;
+	glm::vec3 from(1.0, 4.0, 9.2);
+	glm::vec3 to(4.0, 1.0, 9.8);
+	result = interpolateThreeElementValues(from, to, 4);
+	for(size_t i=0; i<result.size(); i++) {
+		std::cout << "(" << result[i][0] << "," << result[i][1] << "," << result[i][2] << ")" << " ";
+		std::cout << std::endl;
+	}
 
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
